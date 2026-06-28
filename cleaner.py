8b1b94f345579bc05ac5ea4046300ab909bd7559
Irina Voronova <voronovairina905@gmail.com>
2026-06-27 17:46:29 -0700

    add NumPy stats analysis

--- cleaner.py
+++ cleaner.py
@@ -11,3 +11,9 @@ print(data.head())
 
 print("\nMissing values in each column:")
 print(data.isnull().sum())
+
+print("\n--- Dataset Stats --- ")
+print("Average Age:", np.round(data['Age'].mean(), 1))
+print("Survival Rate:", np.round(data['Survived'].mean() * 100, 1), "%")
+print("Oldest Passenger Age:", data['Age'].max())
+print("Youngest Passenger Age:", data['Age'].min())
