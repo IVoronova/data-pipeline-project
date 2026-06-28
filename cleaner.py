9f958aacba0d2ce9f0f5a7f9912af58ba7ff061b
Irina Voronova <voronovairina905@gmail.com>
2026-06-27 18:14:53 -0700

    add survial bar and pie charts visualization

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load the dataset
 data = pd.read_csv('titanic.csv')
@@ -17,3 +18,20 @@ print("Average Age:", np.round(data['Age'].mean(), 1))
 print("Survival Rate:", np.round(data['Survived'].mean() * 100, 1), "%")
 print("Oldest Passenger Age:", data['Age'].max())
 print("Youngest Passenger Age:", data['Age'].min())
+
+# Survival count bar chart
+data['Survived'].value_counts().plot(kind='bar', color=['red', 'green'])
+plt.title('Titanic Survival Count')
+plt.xticks([0,1], ["Died", "Survived"], rotation=0)
+plt.ylabel('Number of Passengers')
+plt.savefig('titanic_survival_chart.png')
+print("\nSurvival count bar chart saved as 'titanic_survival_chart.png'!")
+plt.show()
+
+# Survival % pie chart
+data['Survived'].value_counts().sort_index().plot(kind='pie', labels=['Died', 'Survived'], colors=['red', 'green'], autopct="%1.1f%%")
+plt.title('Titanic Survival Percentage')
+plt.ylabel('')
+plt.savefig('titanic_survival_pie_chart.png')
+print("\nSurvival percentage pie chart saved as 'titanic_survival_pie_chart.png'!")
+plt.show()
